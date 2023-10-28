6dacb11212d5e9854fc662ce9ac8f010753fba7f
XimYa <hj_lee_com@naver.com>
2023-10-28 13:34:58 +0900

    refactor : regex모듈 일부 수정

--- lib/src/regex.dart
+++ lib/src/regex.dart
@@ -1,17 +1,21 @@
-import 'package:simple_nickname_validation_implementation/src/string_extension.dart';
+import 'package:korean_profanity_filter/korean_profanity_filter.dart';
 
-class Regex {
+abstract class Regex {
+  Regex._();
+
   // 공백 존재 여부
   static bool hasSpaceOnString(String value) {
-    return RegExp(r'\s').hasMatch(value.trim()) || value.getLastCharacter == ' '
-        ? true
-        : false;
+    return RegExp(r'\s').hasMatch(value);
   }
 
   /// 적합한 문자 사용 여부
   /// 한글, 알파벳, 숫자, 언더스코어(_), 하이픈(-)만 사용할 수 있음
   static bool hasProperCharacter(String value) {
     return !RegExp(r'^[a-zA-Z0-9ㄱ-ㅎ가-힣_-]+$').hasMatch(value.trim());
+  }
+
+  static bool hasFWords(String value) {
+    return value.containsBadWords;
   }
 
   // Operation 글자 포함 여부
